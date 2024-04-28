7b5c6be4f0ccc648f8cc923bd8f4ca9b3895701e
Freshka <Fresh45rus@gmail.com>
2024-04-28 22:55:38 +0500

    SettingsTopBar

---
+++ app/src/main/java/com/example/omgupsandroidapp/presentation/ui/SettingsScreen/copmposable/SettingsTopAppBar.kt
@@ -0,0 +1,39 @@
+package com.example.omgupsandroidapp.presentation.ui.SettingsScreen.copmposable
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import com.example.omgupsandroidapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsTopAppBar(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(title = {
+        Text(
+            text = stringResource(R.string.Settings),
+            style = MaterialTheme.typography.displayMedium,
+        )
+
+    },
+        navigationIcon = {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = "arrow back"
+                )
+            }
+        },
+        modifier = modifier
+    )
+}

7e4380820573a12dad2cf7c84e6b911750ed44bf
CocomiLA <ttz002@mail.ru>
2024-05-21 20:59:56 +0600

    история

--- app/src/main/java/com/example/omgupsandroidapp/presentation/ui/screens/SheduleScreen/ScheduleScreen.kt
+++ app/src/main/java/com/example/omgupsandroidapp/presentation/ui/screens/SheduleScreen/ScheduleScreen.kt
@@ -1,15 +1,23 @@
 package com.omgupsapp.presentation.ui.SheduleScreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
@@ -30,18 +38,33 @@ fun ScheduleScreen(
         sheduleViewModul.getShedule()
     }
 
-    Column(
+
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Schedule screen")
+        item {
 
-        if(sheduleState.value.sheduleList.isNotEmpty()){
-            sheduleState.value.sheduleList.map {
-                Text(text = it.subj)
-                Text(text = it.day_of_week)
-                Text(text = it.type_of_week.toString())
+            if (sheduleState.value.sheduleList.isNotEmpty()) {
+                sheduleState.value.sheduleList.map {
+                    Box(
+                        modifier = Modifier.background(color = Color.Black)
+                    ) {
+                        Text(text = it.id.toString() + "айди")
+                        /*Text(text = it.subj + "предмет")
+                        Spacer(modifier = Modifier.padding(0.dp,10.dp))
+                        Text(text = it.day_of_week + "день недели")
+                        Spacer(modifier = Modifier.padding(0.dp,10.dp))
+                        Text(text = it.type_of_week.toString() + "неделя")
+                        Spacer(modifier = Modifier.padding(0.dp,10.dp))
+                        Text(text = it.type_of_week.toString() + "неделя")
+                        Spacer(modifier = Modifier.padding(0.dp,10.dp))
+                        Text(text = it.time.toString() + "какая пара")
+                        Spacer(modifier = Modifier.padding(0.dp,10.dp))*/
+                    }
+                    Spacer(modifier = Modifier.padding(0.dp,20.dp))
+                }
             }
         }
     }

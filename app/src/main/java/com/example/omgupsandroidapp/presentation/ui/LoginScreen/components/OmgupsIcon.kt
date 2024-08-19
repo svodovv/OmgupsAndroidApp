d7f388ec06732d4d13744cd76f40278beb62b10d
svodovv <90703451+svodovv@users.noreply.github.com>
2024-08-20 00:15:17 +0600

    Added new UI for auth screen

---
+++ app/src/main/java/com/example/omgupsandroidapp/presentation/ui/LoginScreen/components/OmgupsIcon.kt
@@ -0,0 +1,36 @@
+package com.example.omgupsandroidapp.presentation.ui.screens.LoginScreen.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.omgupsandroidapp.R
+
+@Composable
+fun OmgupsIcon(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.onSecondary)
+    ) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Icon(
+                painter = painterResource(id = R.drawable.omgups_icon_150),
+                contentDescription = stringResource(
+                    id = R.string.omgupslogo
+                ),
+                modifier = Modifier,
+                tint = MaterialTheme.colorScheme.surfaceVariant
+            )
+        }
+    }
+}

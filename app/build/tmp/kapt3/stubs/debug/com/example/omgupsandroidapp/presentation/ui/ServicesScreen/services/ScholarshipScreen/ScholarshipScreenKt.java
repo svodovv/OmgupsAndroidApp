ff401854f689f9dec00ba233f8fef12a500e2790
svodovv <90703451+svodovv@users.noreply.github.com>
2024-08-20 02:14:05 +0600

    added UI for orders

--- app/build/tmp/kapt3/stubs/debug/com/example/omgupsandroidapp/presentation/ui/ServicesScreen/services/ScholarshipScreen/ScholarshipScreenKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/omgupsandroidapp/presentation/ui/ServicesScreen/services/ScholarshipScreen/ScholarshipScreenKt.java
@@ -2,14 +2,16 @@ package com.example.omgupsandroidapp.presentation.ui.ServicesScreen.services.Sch
 
 import androidx.compose.foundation.ExperimentalFoundationApi;
 import androidx.compose.foundation.layout.Arrangement;
+import androidx.compose.material3.CardDefaults;
 import androidx.compose.runtime.Composable;
 import androidx.compose.ui.Alignment;
 import androidx.compose.ui.Modifier;
 import androidx.compose.ui.text.TextStyle;
 import androidx.compose.ui.text.style.TextAlign;
 import androidx.navigation.NavController;
+import com.example.omgupsandroidapp.R;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000.\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\"\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u0007H\u0007\u001a$\u0010\b\u001a\u00020\u00012\u0006\u0010\t\u001a\u00020\n2\b\b\u0002\u0010\u000b\u001a\u00020\f2\b\b\u0002\u0010\r\u001a\u00020\u000eH\u0007\u00a8\u0006\u000f"}, d2 = {"ScholarshipScreen", "", "navController", "Landroidx/navigation/NavController;", "paddingValues", "Landroidx/compose/foundation/layout/PaddingValues;", "scholarshipViewModel", "Lcom/example/omgupsandroidapp/presentation/ui/ServicesScreen/services/ScholarshipScreen/ScholarshipViewModel;", "TextInLazyColumn", "columnName", "", "modifier", "Landroidx/compose/ui/Modifier;", "style", "Landroidx/compose/ui/text/TextStyle;", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u00000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\u001a\"\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u0007H\u0007\u001a$\u0010\b\u001a\u00020\u00012\u0006\u0010\t\u001a\u00020\n2\b\b\u0002\u0010\u000b\u001a\u00020\f2\b\b\u0002\u0010\r\u001a\u00020\u000eH\u0007\u001a\u0010\u0010\u000f\u001a\u00020\u00012\u0006\u0010\u0010\u001a\u00020\nH\u0003\u00a8\u0006\u0011"}, d2 = {"ScholarshipScreen", "", "navController", "Landroidx/navigation/NavController;", "paddingValues", "Landroidx/compose/foundation/layout/PaddingValues;", "scholarshipViewModel", "Lcom/example/omgupsandroidapp/presentation/ui/ServicesScreen/services/ScholarshipScreen/ScholarshipViewModel;", "TextInLazyColumn", "columnName", "", "modifier", "Landroidx/compose/ui/Modifier;", "style", "Landroidx/compose/ui/text/TextStyle;", "TextWithDynamicLineBreak", "text", "app_debug"})
 public final class ScholarshipScreenKt {
     
     @kotlin.OptIn(markerClass = {androidx.compose.foundation.ExperimentalFoundationApi.class})
@@ -18,6 +20,10 @@ public final class ScholarshipScreenKt {
     androidx.navigation.NavController navController, @org.jetbrains.annotations.NotNull
     androidx.compose.foundation.layout.PaddingValues paddingValues, @org.jetbrains.annotations.NotNull
     com.example.omgupsandroidapp.presentation.ui.ServicesScreen.services.ScholarshipScreen.ScholarshipViewModel scholarshipViewModel) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    private static final void TextWithDynamicLineBreak(java.lang.String text) {
     }
     
     @androidx.compose.runtime.Composable
--- app/src/main/java/com/example/omgupsandroidapp/presentation/ui/ServicesScreen/services/ScholarshipScreen/ScholarshipScreen.kt
+++ app/src/main/java/com/example/omgupsandroidapp/presentation/ui/ServicesScreen/services/ScholarshipScreen/ScholarshipScreen.kt
@@ -1,30 +1,41 @@
 package com.example.omgupsandroidapp.presentation.ui.ServicesScreen.services.ScholarshipScreen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.omgupsandroidapp.R
 import com.example.omgupsandroidapp.presentation.ui.LoadingScreen.LoadingScreen
 import com.example.omgupsandroidapp.presentation.ui.ServicesScreen.services.ServicesTopAppBar
 
@@ -46,97 +57,141 @@ fun ScholarshipScreen(
         ServicesTopAppBar(title = "Выплаты", navController = navController)
 
         if (scholarship.value.scholarshipList.isNotEmpty()) {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                stickyHeader {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = 8.dp, end = 8.dp)
-                    ) {
-                        TextInLazyColumn(
-                            columnName = "Период",
-                            modifier = Modifier.weight(1.5f),
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        TextInLazyColumn(
-                            columnName = "Тип выплаты",
-                            modifier = Modifier.weight(1.7f),
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        TextInLazyColumn(
-                            columnName = "Курс",
-                            modifier = Modifier.weight(1.2f),
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        TextInLazyColumn(
-                            columnName = "Сумма",
-                            modifier = Modifier.weight(0.7f),
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                    }
-                    Divider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 8.dp),
-                        color = Color.Black,
-                        thickness = 1.dp
-                    )
+            Card(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondary)
+            ) {
+                var isVisible by remember {
+                    mutableStateOf(true)
                 }
-
-                itemsIndexed(scholarship.value.scholarshipList) { index, it ->
-                    Row(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(horizontal = 8.dp)
-                            .background(
-                                color = if (index % 2 == 0) {
-                                    MaterialTheme.colorScheme.background
-                                } else {
-                                    MaterialTheme.colorScheme.surfaceVariant
-                                }
-                            ), verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        TextInLazyColumn(
-                            columnName = it.dateInterval, modifier = Modifier.weight(1.5f)
-                        )
-                        TextInLazyColumn(
-                            columnName = it.typeOfPayment, modifier = Modifier.weight(1.7f)
-                        )
-                        TextInLazyColumn(columnName = it.year, modifier = Modifier.weight(1.2f))
-                        TextInLazyColumn(columnName = it.sum, modifier = Modifier.weight(0.7f))
-                    }
-                    Divider(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 8.dp),
-                        color = MaterialTheme.colorScheme.onSurfaceVariant,
-                        thickness = 1.dp
-                    )
-                }
-                scholarship.value.totalSum?.let {
-                    item {
+                LazyColumn(modifier = Modifier) {
+                    stickyHeader {
                         Row(
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 8.dp),
-                            horizontalArrangement = Arrangement.End,
+                                .fillMaxSize()
+                                .padding(start = 8.dp, end = 8.dp),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Text(
-                                text = "Общая сумма: ${it.totalSum}",
-                                style = MaterialTheme.typography.bodySmall,
-                                textAlign = TextAlign.Start
+                            TextWithDynamicLineBreak(
+                                text = stringResource(R.string.pgas)
                             )
+                            Spacer(
+                                modifier = Modifier.weight(1f)
+                            )
+                            IconButton(onClick = { isVisible = !isVisible }) {
+                                Icon(
+                                    painter = painterResource(
+                                        id = if (isVisible) R.drawable.baseline_keyboard_arrow_down_24
+                                        else R.drawable.baseline_keyboard_arrow_up_24
+                                    ),
+                                    contentDescription = stringResource(R.string.arrow_in_order_card),
+                                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            }
                         }
                     }
-                }
+                    stickyHeader {
+                        AnimatedVisibility(isVisible) {
 
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(start = 8.dp, end = 8.dp)
+                            ) {
+                                TextInLazyColumn(
+                                    columnName = "Период",
+                                    modifier = Modifier.weight(1.5f),
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+
+                                TextInLazyColumn(
+                                    columnName = "Курс",
+                                    modifier = Modifier.weight(1.2f),
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                                TextInLazyColumn(
+                                    columnName = "Сумма",
+                                    modifier = Modifier.weight(0.7f),
+                                    style = MaterialTheme.typography.bodySmall
+                                )
+                            }
+
+                        }
+                    }
+
+                    itemsIndexed(scholarship.value.scholarshipList) { index, it ->
+                        AnimatedVisibility(isVisible) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                TextInLazyColumn(
+                                    columnName = it.dateInterval, modifier = Modifier.weight(1.5f)
+                                )
+
+                                TextInLazyColumn(
+                                    columnName = it.year, modifier = Modifier.weight(1.2f)
+                                )
+                                TextInLazyColumn(
+                                    columnName = it.sum, modifier = Modifier.weight(0.7f)
+                                )
+                            }
+                        }
+                    }
+                    scholarship.value.totalSum?.let {
+                        item {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .then(
+                                        if (isVisible) Modifier.padding(
+                                            horizontal = 8.dp, vertical = 16.dp
+                                        )
+                                        else Modifier.padding(bottom = 8.dp)
+                                    ),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = "Общая сумма: ${it.totalSum}",
+                                    style = MaterialTheme.typography.bodySmall,
+                                    textAlign = TextAlign.Start,
+                                    color = MaterialTheme.colorScheme.onBackground
+                                )
+                            }
+                        }
+                    }
+
+                }
             }
         } else {
             LoadingScreen()
         }
 
     }
+}
+
+@Composable
+private fun TextWithDynamicLineBreak(text: String) {
+    // Разбиваем текст на слова
+    val words = text.split(" ")
+
+    // Если слов больше одного, переносим текст после первого слова
+    val textWithLineBreak = if (words.size > 1) {
+        words[0] + "\n" + words.subList(1, words.size).joinToString(" ")
+    } else {
+        text // Если только одно слово, возвращаем текст как есть
+    }
+
+    Text(
+        text = textWithLineBreak,
+        style = MaterialTheme.typography.bodySmall,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+    )
 }
 
 @Composable
@@ -149,7 +204,10 @@ fun TextInLazyColumn(
         modifier = modifier, contentAlignment = Alignment.Center
     ) {
         Text(
-            text = columnName, style = style, textAlign = TextAlign.Center
+            text = columnName,
+            style = style,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onBackground
         )
     }
 }

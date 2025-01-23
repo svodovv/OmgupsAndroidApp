ad6ede7994bb0db4ec9cebcce6c993c027e8eaed
COCOmiLA <ttz002@mail.ru>
2025-01-23 22:35:40 +0600

    АЛЬФА ВЕСРСИЯ 1,0,1 RuSdk

--- app/src/main/java/com/example/omgupsandroidapp/presentation/ui/ServicesScreen/components/ServicesScreen.kt
+++ app/src/main/java/com/example/omgupsandroidapp/presentation/ui/ServicesScreen/components/ServicesScreen.kt
@@ -1,5 +1,7 @@
 package com.omgupsapp.presentation.ui.homeScreen.components
 
+import android.content.Context
+import androidx.collection.intFloatMapOf
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.scrollable
@@ -15,21 +17,29 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.example.omgupsandroidapp.presentation.ui.ServicesScreen.ServicesViewModel
+import com.example.omgupsandroidapp.presentation.utils.Event
 import com.example.omgupsandroidapp.presentation.utils.RuStoreUpDateViewModel
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -37,9 +47,11 @@ fun ServiceScreen(
     navController: NavController,
     paddingValues: PaddingValues,
     servicesViewModel: ServicesViewModel = hiltViewModel(),
-    uStoreUpDateViewModel : RuStoreUpDateViewModel = hiltViewModel()
+    ruStoreUpDateViewModel : RuStoreUpDateViewModel = hiltViewModel()
 ) {
     val serviceList = servicesViewModel.serviceList
+    val events by ruStoreUpDateViewModel.events.collectAsState(initial = null)
+
 
     LazyColumn(
         modifier = Modifier
@@ -48,6 +60,7 @@ fun ServiceScreen(
             //.verticalScroll(rememberScrollState())
             .padding(paddingValues)
     ) {
+
 
         serviceList.forEach {
             item {
@@ -92,6 +105,25 @@ fun ServiceScreen(
                 }
             }
         }
-        //Spacer(modifier = Modifier.size(150.dp))
+        ruStoreUpDateViewModel.viewModelScope.launch {
+            ruStoreUpDateViewModel.ruStoreAppUpdateManager.getAppUpdateInfo()
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        //Text(text = "Статус обновления: $downloadProgress%")
+
+        //Button(onClick = { viewModel.init(LocalContext.current) }) {
+            Text(text = "Проверить обновление")
+       // }
+
+        if (events is Event.UpdateCompleted) {
+            Text(text = "Обновление завершено!", color = Color.Green)
+        }
     }
 }
--- app/src/main/java/com/example/omgupsandroidapp/presentation/utils/UpDateRuStoreSdk.kt
+++ app/src/main/java/com/example/omgupsandroidapp/presentation/utils/UpDateRuStoreSdk.kt
@@ -19,7 +19,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RuStoreUpDateViewModel @Inject constructor() : ViewModel() {
-    private lateinit var ruStoreAppUpdateManager: RuStoreAppUpdateManager
+    lateinit var ruStoreAppUpdateManager: RuStoreAppUpdateManager
     private val _events = MutableSharedFlow<Event>(
         extraBufferCapacity = 1,
         onBufferOverflow = BufferOverflow.DROP_OLDEST
@@ -47,6 +47,7 @@ class RuStoreUpDateViewModel @Inject constructor() : ViewModel() {
     fun init(context: Context) {
         // В данном примере подобное создание класса сделано для упрощения. В реальных проектах
         // рекомендуеся использовать DI
+        val updateManager = RuStoreAppUpdateManagerFactory.create(context)
         ruStoreAppUpdateManager = RuStoreAppUpdateManagerFactory.create(context)
         ruStoreAppUpdateManager
             .getAppUpdateInfo()
